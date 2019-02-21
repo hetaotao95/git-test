9802a0a68d7b1ea26a23ce7bf5619587b8da5dab
hetaotao <1432871873@qq.com>
2019-02-21 15:56:21 +0800

    修改firstclass类

--- git-test/src/cn/itcast/FirstClass.java
+++ git-test/src/cn/itcast/FirstClass.java
@@ -1,6 +1,6 @@
 package cn.itcast;
 
-public class FirstClass {
+public class  FirstClass {
     public static void main(String[] args) {
         System.out.println("hello git....");
         System.out.println("hello git....");
@@ -11,6 +11,6 @@ public class FirstClass {
     public void  test(){
         int i = 0;
         i += 10;
-
+        System.out.println(i);
     }
 }
