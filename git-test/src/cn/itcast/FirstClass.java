b1cac65793e3a1c9d8c44e4adfa5a9461aa80f17
hetaotao <1432871873@qq.com>
2019-02-21 15:50:42 +0800

    将本地项目添加到git

---
+++ git-test/src/cn/itcast/FirstClass.java
@@ -0,0 +1,16 @@
+package cn.itcast;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git....");
+        System.out.println("hello git....");
+        System.out.println("hello git....");
+        System.out.println("hello git....");
+
+    }
+    public void  test(){
+        int i = 0;
+        i += 10;
+
+    }
+}
